f3b6ad08469d27b241f8501dcf684aeb605b1e86
Dave Hamersma <d.hamersma@sensire.nl>
2023-05-01 18:32:30 +0200

    make_dataset file gemaakt, mist alleen nog missi

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,16 +2,65 @@
 import os
 import click
 import logging
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import numpy as np
 
 
+#input_filepath = pd.read_parquet("data/raw/knee-provider.parquet")
+#output_filepath = "./data/processed/output_dataset.csv"
+
+
+def type_fixer(df):
+    ## alle types naar de goede format zetten
+    df = df.provider_code.to_string() #dit is een unieke identifier, dus string
+    return df
+
+def missing_values(df):
+    ## missing values worden hier verwijderd of aangepast
+    return df
+
+def near_zero_variances(df):
+    ## Eerst de numerical
+    from sklearn.feature_selection import VarianceThreshold
+    selector = VarianceThreshold() #threshold van 0
+    selector.fit(df.select_dtypes('number'))
+    out = selector.get_support()
+    out2 = [not elem for elem in out]
+    vars_drop = list(df.select_dtypes('number').columns[out2])
+        
+    # String vars toevoegen
+    vars_drop.append("procedure") #deze string column bevat alleen dezelfde strings
+    
+    # Verwijderen columns
+    return df.drop(vars_drop, axis=1)
+
+
+def nieuwe_vars(df):
+    ## Hier worden nieuwe variabelen aangemaakt
+    df['oks_change_score'] = df.oks_t1_score - df.oks_t0_score
+    df['oks_MID_5'] = np.where((df.oks_change_score >= 5), 'CHANGE','NO_CHANGE') #Als definitie van MID 15 zou zijn. Dit nog onderbouwen met lit.
+    df['oks_MID_7'] = np.where((df.oks_change_score >= 7), 'CHANGE','NO_CHANGE')
+    df['oks_MID_10'] = np.where((df.oks_change_score >= 10), 'CHANGE','NO_CHANGE')
+    df['succesfaction'] = np.where((df.t1_sucess > 3) & (df.t1_satisfaction > 4) & (df.oks_MID_5 == 'NO_CHANGE'), 'negatief_advies', 'positief_advies')
+    df['succesfaction_or'] = np.where((df.t1_sucess > 3) | (df.t1_satisfaction > 4) | (df.oks_MID_5 == 'NO_CHANGE'), 'negatief_advies', 'positief_advies')
+    
+    return df
+    
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
+
 def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
+
+    df = pd.read_parquet(input_filepath)
+    
+    df = type_fixer(df)
+    df = missing_values(df)
+    df = near_zero_variances(df)
+    df = nieuwe_vars(df)
+    
+    df.to_csv(output_filepath)
+    
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
@@ -19,12 +68,5 @@ def main(input_filepath, output_filepath):
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
     main()
+    
