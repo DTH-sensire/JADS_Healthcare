2e460e64190243fe7fe701af25729ab8c8c3fcbe
Dave Hamersma <d.hamersma@sensire.nl>
2023-05-25 16:05:43 +0200

    initial commit + verdere uitwerking model

---
+++ src/models/feature_selection.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+
+# Read data
+df = pd.read_parquet("C:/Users/Dave/Desktop/JADS/JADS_project/JADS_Healthcare/data/processed/output_dataset.parquet")
+
+df = df.replace('positief_advies', 0)
+df = df.replace('negatief_advies', 1)
+
+def type_fixer(df):
+    ## alle types naar de goede format zetten
+    
+    uint8 = list(df.select_dtypes('uint8').columns)
+    uint16 = list(df.select_dtypes('uint16').columns)
+    
+    to_cat2 = list(df.columns[df.columns.str.contains("t0")])
+    numericals = ['t0_eq5d_index', 't0_eq_vas', 'oks_t0_score']
+    to_cat2 = [element for element in to_cat2 if element not in numericals] 
+    naar_cat = uint8 + uint16 + to_cat2
+    
+    for col in naar_cat:
+        df[col] = df[col].astype('category')
+    
+    return df
+
+df = type_fixer(df)
+
+
+## Train test
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(df.drop(['succesfaction_or', 'succesfaction_and'], axis=1), df['succesfaction_or'], test_size=0.33, random_state=42, stratify=df['succesfaction_or'])
+
+## One Hot Encoding
+cats = list(X_train.select_dtypes('category').columns)
+X_train = pd.get_dummies(X_train, columns=cats)
+
+cats = list(X_test.select_dtypes('category').columns)
+X_test = pd.get_dummies(X_test, columns=cats)
+
+## Model instance om mee te testen
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier(n_estimators=100, random_state=None, class_weight='balanced_subsample')
+
+## Univariate feature selection
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+
+BestFeatures = SelectKBest(score_func=chi2, k=10)
+fit = BestFeatures.fit(X_train.drop(["t0_eq5d_index", "year", "age_band"], axis=1),y_train) #moet wel zonder eq5d_index
+
+df_scores = pd.DataFrame(fit.scores_)
+df_columns = pd.DataFrame(X_train.drop(["t0_eq5d_index", "year", "age_band"], axis=1).columns)
+f_Scores = pd.concat([df_columns,df_scores],axis=1)
+f_Scores.columns = ['Specs','Score']
+print(f_Scores.nlargest(20,'Score')) 
+
+## Wrapper methods
+from mlxtend.feature_selection import SequentialFeatureSelector as SFS
+
+sfs1 = SFS(model, 
+           k_features=10, 
+           forward=True, 
+           floating=False, 
+           verbose=2,
+           scoring='r2',
+           cv=3)
+
+sfs1 = sfs1.fit(np.array(X_train), y_train)
+
+X_train.columns[list(sfs1.k_feature_idx_)]
+
+
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,5 +1,6 @@
 #Train model
 import pandas as pd
+import numpy as np
 
 
 # Read data
@@ -39,6 +40,119 @@ X_train = pd.get_dummies(X_train, columns=cats)
 cats = list(X_test.select_dtypes('category').columns)
 X_test = pd.get_dummies(X_test, columns=cats)
 
+
+## Feature selection
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import itertools
+from sklearn import preprocessing
+from sklearn import model_selection
+from sklearn.metrics import confusion_matrix, make_scorer, average_precision_score 
+from sklearn.model_selection import learning_curve, StratifiedKFold, train_test_split
+from sklearn.feature_selection import RFECV, SelectFromModel
+from sklearn.ensemble import RandomForestClassifier
+from eli5.sklearn import PermutationImportance
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    Plots a learning curve. http://scikit-learn.org/stable/modules/learning_curve.html
+    """
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+    plt.legend(loc="best")
+    return plt
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+dict_characters = {0: 'Positief_advies', 1: 'Negatief_advies'}
+
+def runRandomForest(a, b, c, d):
+    model = RandomForestClassifier(n_estimators=100, random_state=None, class_weight='balanced_subsample')
+    precision_scorer = make_scorer(average_precision_score)
+    model.fit(a, b)
+    kfold = model_selection.KFold(n_splits=10, random_state=None)
+    precision = model_selection.cross_val_score(model, c, d, cv=kfold, scoring='precision')
+    mean = precision.mean() 
+    stdev = precision.std()
+    prediction = model.predict(c)
+    cnf_matrix = confusion_matrix(d, prediction)
+    #plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,title='Normalized confusion matrix')
+    plot_learning_curve(model, 'Learning Curve For RandomForestClassifier', a, b, (0.80,1.1), 10)
+    plt.show()
+    plot_confusion_matrix(cnf_matrix, classes=dict_characters,title='Confusion matrix')
+    plt.show()
+    print('Random Forest Classifier - Training set accuracy: %s (%s)' % (mean, stdev))
+    return
+runRandomForest(X_train, y_train, X_test, y_test)
+
+model = RandomForestClassifier(n_estimators=100, random_state=None, class_weight='balanced_subsample')
+model.fit(X_train,y_train)
+columns = X_train.columns
+coefficients = model.feature_importances_.reshape(X_train.columns.shape[0], 1)
+absCoefficients = abs(coefficients)
+fullList = pd.concat((pd.DataFrame(columns, columns = ['Variable']), pd.DataFrame(absCoefficients, columns = ['absCoefficient'])), axis = 1).sort_values(by='absCoefficient', ascending = False)
+print('RandomForestClassifier - Feature Importance:')
+print('\n',fullList,'\n')
+
+X_train=np.asarray(X_train)
+X_test=np.asarray(X_test)
+Y_train=np.asarray(y_train)
+Y_test=np.asarray(y_test)
+
+# BEGIN: FEATURE SELECTION WITH PERMUTATION IMPORTANCE METHOD
+#http://eli5.readthedocs.io/en/latest/blackbox/permutation_importance.html
+sel = SelectFromModel(PermutationImportance(RandomForestClassifier(n_estimators=10, random_state=None, class_weight='balanced_subsample'), cv=5),threshold=0.005,).fit(X_train, Y_train)
+X_train2 = sel.transform(X_train)
+X_test2 = sel.transform(X_test)
+# END: FEATURE SELECTION WITH PERMUTATION IMPORTANCE METHOD
+
+runRandomForest(X_train2, Y_train, X_test2, Y_test)
+
+
 ## Naive classifier
 from sklearn.dummy import DummyClassifier
 model_naive = DummyClassifier(strategy='stratified') #random class
@@ -55,12 +169,22 @@ from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
 
 # define model
-model_rf = RandomForestClassifier(n_estimators=100, random_state=42)
+model_rf = RandomForestClassifier(n_estimators=100, random_state=42, class_weight='balanced_subsample')
 model_rf.fit(X_train, y_train)
 preds = model_rf.predict(X_test)
 
+# performance model
+kfold = model_selection.KFold(n_splits=10, random_state=None)
+precision = model_selection.cross_val_score(model, X_test, y_test, cv=kfold, scoring='precision')
+mean = precision.mean()
+stdev = precision.std()
+prediction = model_rf.predict(X_test)
+
+plot_learning_curve(model_rf, 'Learning Curve For RandomForestClassifier', X_train, y_train, (0.80,1.1), 10)
+plt.show()
+
 # Accuracy van model
-cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=42)
 scoring = ["precision"]
 cv_result = cross_validate(model_rf, X_train, y_train, scoring=scoring)
 print(f"Precision: {cv_result['test_precision'].mean():.3f}") # Precision 0, omdat hij nooit een 1 predict. Zie:
@@ -68,7 +192,7 @@ set(y_test) - set(preds)
 
 from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
-out = ConfusionMatrixDisplay.from_predictions(y_test, preds, display_labels=['Positief_advies', 'Negatief_advies']).plot()
+ConfusionMatrixDisplay.from_predictions(y_test, prediction, display_labels=['Positief_advies', 'Negatief_advies']).plot()
 plt.show() 
 
 
